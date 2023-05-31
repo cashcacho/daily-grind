6f5ef9c4a586808a71e3a34f9672c5ec5e3f2897
Justine Cacho <justinecacho@gmail.com>
2023-05-31 13:43:23 -0700

    updates

---
+++ js/daily-grind.js
@@ -0,0 +1,49 @@
+    /*
+        Here are the items we believe we'll need for each day's coffee
+
+        pic - image src
+        alt - the alt tag for the image
+        color - color to match image
+        desc - description of coffee
+        name - name of the coffee
+        day - the day of the week
+
+    */
+
+
+myDate = new Date();
+
+today = myDate.getDay();
+
+//today=3;
+
+switch(today){
+
+    case 2:
+        today = "Tuesday";
+    break;
+
+    case 3:
+        today = "Wednesday";
+    break;
+
+    default:
+        today = "Not sure what day it is!";
+
+}
+
+coffee = {
+    name:"Bubble Tea",
+    color: "pink",
+    pic: "images/bubble-tea.jpg",
+    alt: "A picture of a bubble tea",
+    day: "Wednesday",
+    desc: 'I like me some bubble tea!'
+}
+
+
+
+
+
+//alert(today);
+console.log(coffee);
